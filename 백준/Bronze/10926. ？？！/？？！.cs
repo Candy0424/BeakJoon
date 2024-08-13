df8dcf179c122cbb54fa72bec5b97de88f5e1a03
candy <157550346+Candy0424@users.noreply.github.com>
2024-08-14 01:31:27 +0900

    [Bronze V] Title: ??!, Time: 60 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace beakjoon
+{
+    class 백준
+    {
+        static void Main()
+        {
+            string id = Console.ReadLine();
+            
+            if(id == id)
+            {
+                Console.WriteLine(id + "??!");
+            }
+        }
+    }
+}
