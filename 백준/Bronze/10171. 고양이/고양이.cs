ddee4791d12fe56e362dc0e8d8fd500e0a5ba4a5
candy <157550346+Candy0424@users.noreply.github.com>
2024-08-14 01:31:15 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5304 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace beakjoon
+{
+    class 백준
+    {
+        static void Main()
+        {
+            Console.Write("\\");
+            Console.WriteLine("    /\\");
+            Console.WriteLine(" )  ( ') ");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
