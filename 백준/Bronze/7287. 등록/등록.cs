caeac9c55c962b598c0e6ff37482c1e05c34cd12
candy <157550346+Candy0424@users.noreply.github.com>
2024-08-14 01:31:09 +0900

    [Bronze V] Title: 등록, Time: 48 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace beakjoon
+{
+    class 백준
+    {
+        static void Main()
+        {
+            Console.WriteLine("10");
+            Console.WriteLine("dltkr0424");
+        }
+    }
+}
