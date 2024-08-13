598b9dcf3883d4e185115b5f5fdc0dc36c15be4c
candy <157550346+Candy0424@users.noreply.github.com>
2024-08-14 01:32:01 +0900

    [Bronze V] Title: 꼬마 정민, Time: 60 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,8 @@
+string n = Console.ReadLine();
+            string[] nn = n.Split(' ');
+
+            long A = long.Parse(nn[0]);
+            long B = long.Parse(nn[1]);
+            long C = long.Parse(nn[2]);
+
+            Console.WriteLine(A + B + C);
