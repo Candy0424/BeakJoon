f2048ebf1810bdd0c32924df68933e578b5a2067
candy <157550346+Candy0424@users.noreply.github.com>
2024-08-14 01:31:46 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 88 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cs
@@ -0,0 +1,8 @@
+int n = int.Parse(Console.ReadLine()); // 24 가정
+
+            for (int i = 0; i < n/4; i++)
+            {
+                    Console.Write("long" + " ");
+            }
+
+            Console.Write("int");
