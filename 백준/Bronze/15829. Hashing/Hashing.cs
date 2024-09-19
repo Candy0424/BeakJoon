37efc8a110129f86f7b39eb8d26b8c4651edbd84
candy <157550346+Candy0424@users.noreply.github.com>
2024-09-19 17:04:32 +0900

    [Bronze II] Title: Hashing, Time: 56 ms, Memory: 5912 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cs
+++ 백준/Bronze/15829. Hashing/Hashing.cs
@@ -1,36 +1,65 @@
-int r = 31;
-int m = 1234567891;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.IO;
+using System.Text;
+using System.CodeDom;
+using System.Collections;
 
-int l = int.Parse(Console.ReadLine());
 
-int num = 0;
-
-List<int> nums = new List<int>();
-
-int p = 0;
-
-int hashing = 0;
-
-int value = 0;
-
-string input = Console.ReadLine().ToLower();
-
-foreach (char item in input)
+namespace BeakJoon
 {
-    num = 0;
-    for (char i = 'a'; i <= item; i++)
+    internal class Program
     {
-        num++;
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int r = 31;
+            BigInteger m = 1234567891;
+
+            BigInteger l = BigInteger.Parse(Console.ReadLine());
+
+            BigInteger value;
+
+            int num;
+
+            List<int> nums = new List<int>();
+
+            BigInteger hashing = 0;
+
+            string input = Console.ReadLine();
+
+            foreach (char item in input)
+            {
+                num = 0;
+                for (char i = 'a'; i <= item; i++)
+                {
+                    num++;
+                }
+                nums.Add(num);
+            }
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == 0)
+                {
+                    value = 1;
+                }
+                else
+                {
+                    value = r;
+                }
+                for (int j = 2; j <= i; j++)
+                {
+                    value *= r;
+                }
+                hashing += (nums[i] * value);
+            }
+            Console.WriteLine(hashing % m);
+        }
     }
-    nums.Add(num);
 }
-
-for (int i = 0; i < input.Length; i++)
-{
-    value = r;
-
-    double pow = Math.Pow(value, i);
-    hashing += nums[i] * Convert.ToInt32(pow);
-}
-
-Console.WriteLine(hashing % m);
