6361445906c4b9cc7524d0fa3502d274f8bed20b
candy <157550346+Candy0424@users.noreply.github.com>
2025-03-17 09:24:07 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num;
+
+	cin >> num;
+
+	for (int i = num; i > 0; i--)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			cout << " ";
+		}
+
+		for (int j = num; j >= i; j--)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+}
