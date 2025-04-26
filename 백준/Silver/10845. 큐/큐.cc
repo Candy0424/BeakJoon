4f438dc689a937b838270a19e3d115c401ef31ee
candy <157550346+Candy0424@users.noreply.github.com>
2025-04-26 20:42:01 +0900

    [Silver IV] Title: 큐, Time: 212 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	queue<int> q;
+
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		int x = 0;
+
+		cin >> str;
+
+		if (str == "push")
+		{
+			cin >> x;
+			q.push(x);
+		}
+		else if (str == "pop")
+		{
+			if (q.size() == 0)
+				cout << -1 << '\n';
+			else
+			{
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (str == "size")
+		{
+			cout << q.size() << '\n';
+		}
+		else if (str == "empty")
+		{
+			if (q.size() == 0)
+				cout << 1 << '\n';
+			else
+				cout << 0 << '\n';
+		}
+		else if (str == "front")
+		{
+			if (q.size() == 0)
+				cout << -1 << '\n';
+			else
+			{
+				cout << q.front() << '\n';				
+			}
+		}
+		else if (str == "back")
+		{
+			if (q.size() == 0)
+				cout << -1 << '\n';
+			else
+			{
+				cout << q.back() << '\n';
+			}
+		}
+	}
+}
