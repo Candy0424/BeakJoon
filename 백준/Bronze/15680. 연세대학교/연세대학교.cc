7d48923fae08d4fbca4f61a1746ed2d57c02a68f
candy <157550346+Candy0424@users.noreply.github.com>
2025-05-06 22:08:30 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int inp;
+    
+    cin >> inp;
+    
+    if (inp == 0)
+        cout << "YONSEI" << endl;
+    else if (inp == 1)
+        cout << "Leading the Way to the Future" << endl;
+}
