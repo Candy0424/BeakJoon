96db9766a9ef327d4796e12eb5b5442e6240067a
candy <157550346+Candy0424@users.noreply.github.com>
2025-05-14 17:03:00 +0900

    [Gold V] Title: 회의실 배정, Time: 28 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -14,32 +14,29 @@ bool Compare(pair<int, int> a, pair<int, int> b)
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n, cnt = 0, curTime = 0;
 
     cin >> n;
 
     vector<pair<int, int>> v(n);
-    vector<bool> eV(n);
 
     for (int i = 0; i < n; ++i)
     {
         cin >> v[i].first >> v[i].second;
-        eV[i] = false;
     }
 
     sort(v.begin(), v.end(), Compare);
-    int minTime = v[0].second, minIdx = 0;
 
     for (int i = 0; i < v.size(); ++i)
     {
-        if (curTime <= v[i].first && eV[i] == false)
+        if (curTime <= v[i].first)
         {
-            minTime = v[i].second;
-            eV[i] = true;
-            minIdx = i;
             cnt++;
-            curTime = minTime;
-            continue;
+            curTime = v[i].second;
         }
     }
 
