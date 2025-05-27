60115e9a2d841b7a15c53b602fd385e903a63561
candy <157550346+Candy0424@users.noreply.github.com>
2025-05-27 21:45:33 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		string str;
+		cin >> str;		
+		int len = str.length();
+
+		if (len >= 6 && len <= 9)
+			cout << "yes\n";
+		else
+			cout << "no\n";
+	}
+}
