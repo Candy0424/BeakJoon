7ffe93519ed87d7b5218e261145b3793def18afa
candy <157550346+Candy0424@users.noreply.github.com>
2025-06-13 17:58:39 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		string str;
+		cin >> str;
+
+		if (str == "Algorithm")
+			cout << "204\n";
+		else if (str == "DataAnalysis")
+			cout << "207\n";
+		else if (str == "ArtificialIntelligence")
+			cout << "302\n";
+		else if (str == "CyberSecurity")
+			cout << "B101\n";
+		else if (str == "Network")
+			cout << "303\n";
+		else if (str == "Startup")
+			cout << "501\n";
+		else
+			cout << "105\n";
+	}
+}
