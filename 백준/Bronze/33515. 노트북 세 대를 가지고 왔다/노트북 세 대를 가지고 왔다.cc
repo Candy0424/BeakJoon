27dc23b1ca4ab8e4722eb940da557fc9ce162200
candy <157550346+Candy0424@users.noreply.github.com>
2025-06-15 04:12:22 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << min(a, b);
+}
