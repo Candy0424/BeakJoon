edb10b06d2f78dc53b44aa965d382c102b43277b
candy <157550346+Candy0424@users.noreply.github.com>
2025-08-09 19:06:07 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    
+    cin >> a >> b;
+    
+    for (int i = 0; i < b; ++i)
+    {
+        int c, d;
+        cin >> c >> d;
+    }
+    
+    cout << "비와이";
+}
