d79ed8301b333f2a824fde85754642f4963f6c4f
candy <157550346+Candy0424@users.noreply.github.com>
2025-08-27 19:57:17 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 108 ms, Memory: 2564 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -3,72 +3,65 @@
 
 using namespace std;
 
-void MakeNode(int& key, vector<int> tree[], int node)
+void insertNode(vector<pair<int, pair<int, int>>>& tree, pair<int, pair<int, int>> target, int idx)
 {
-	tree[node].push_back(key);
-	tree[node].push_back(-1);
-	tree[node].push_back(-1);
-}
-
-void insertNode(int& key, vector<int> tree[], int target)
-{
-	if (tree[0].size() == 0)
+	if (tree.size() == 0)
 	{
-		MakeNode(key, tree, 0);
+		tree.push_back(target);
 		return;
 	}
 
 	while(true)
 	{
-		if (key < tree[target][0])
+		if (target.first < tree[idx].first)
 		{
-			if (tree[target][1] == -1)
+			if (tree[idx].second.first == -1)
 			{
-				MakeNode(key, tree, key);
-				tree[target][1] = key;
+				tree[idx].second.first = tree.size();
+				tree.push_back(target);
 				return;
 			}
 			else
 			{
-				target = tree[target][1];
+				idx = tree[idx].second.first;
 				continue;
 			}
 		}
-		else if (key > tree[target][0])
+		else if (target.first > tree[idx].first)
 		{
-			if (tree[target][2] == -1)
+			if (tree[idx].second.second == -1)
 			{
-				MakeNode(key, tree, key);
-				tree[target][2] = key;
+				tree[idx].second.second = tree.size();
+				tree.push_back(target);
 				return;
 			}
 			else
 			{
-				target = tree[target][2];
+				idx = tree[idx].second.second;
 				continue;
 			}
 		}
 	}
 }
 
-void PostorderTraverse(vector<int> tree[], int a)
+void PostorderTraverse(vector<pair<int, pair<int, int>>>& tree, int a)
 {
-	if (tree[a][1] != -1)
-		PostorderTraverse(tree, tree[a][1]);
-	if (tree[a][2] != -1)
-		PostorderTraverse(tree, tree[a][2]);
-	cout << tree[a][0] << '\n';
+	if (tree[a].second.first != -1)
+		PostorderTraverse(tree, tree[a].second.first);
+	if (tree[a].second.second != -1)
+		PostorderTraverse(tree, tree[a].second.second);
+	cout << tree[a].first << '\n';
 }
 
 int main()
 {
-	vector<int> tree[10000001];
+	vector<pair<int, pair<int, int>>> tree;
 
 	int a;
 
 	while (cin >> a)
 	{
-		insertNode(a, tree, 0);
+		insertNode(tree, { a, {-1, -1} }, 0);
 	}
 	PostorderTraverse(tree, 0);
 }
